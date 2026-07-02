f31a3d1968b4f8e9737f7782ed4598c666b74fca
refilate-spec <refilate@gmail.com>
2026-07-02 21:35:25 +0530

    Implement URL cleaning for specific parameters
    Added logic to clean the URL by removing specific query parameters.

--- webiyox-main.js
+++ webiyox-main.js
@@ -1,1 +1,22 @@
-
+// ?m=1 remove hs code start.. 
+var uri = window.location.toString();
+if (uri.indexOf("%3D","%3D") > 0) {
+var clean_uri = uri.substring(0, uri.indexOf("%3D"));
+window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf("%3D%3D","%3D%3D") > 0) {
+var clean_uri = uri.substring(0, uri.indexOf("%3D%3D"));
+window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf("&m=1","&m=1") > 0) {
+var clean_uri = uri.substring(0, uri.indexOf("&m=1"));
+window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf("?m=1","?m=1") > 0) {
+var clean_uri = uri.substring(0, uri.indexOf("?m=1"));
+window.history.replaceState({}, document.title, clean_uri);
+}
+  // ?m=1 remove hs code end.. 
