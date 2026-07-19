8551a3cd7cb372d423a4c8d463ea4c9e974e7754
refilate-spec <refilate@gmail.com>
2026-07-19 14:28:49 +0530

    Add toggleText function for expanding text

--- webiyox-main.js
+++ webiyox-main.js
@@ -20,3 +20,20 @@ var clean_uri = uri.substring(0, uri.indexOf("?m=1"));
 window.history.replaceState({}, document.title, clean_uri);
 }
   // ?m=1 remove hs code end.. 
+
+function toggleText() {
+  const wrapper = document.getElementById("textWrapper");
+  const btn = document.getElementById("toggleBtn");
+
+  wrapper.classList.toggle("expanded");
+  wrapper.classList.toggle("collapsed");
+
+  if (wrapper.classList.contains("expanded")) {
+    btn.innerHTML = "Show Less";
+  } else {
+    btn.innerHTML = "Show More";
+  }
+}
+
+// show more btn js end 
+
